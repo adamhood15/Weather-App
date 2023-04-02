abe3fd5d4d7bf9ff3109d859912885fe60675f2d
Mr. Hood <adamhood15@gmail.com>
2023-04-02 09:30:38 -0500

    Fixed all errors for application. It is not complete!

--- script.js
+++ script.js
@@ -10,6 +10,39 @@ var fiveDays = [];
 var temp = [];
 var weather = [];
 var wind = [];
+var iconId= [];
+
+//Assigns each weather id code to the specified custom icon in the file
+var weatherIconId = {
+    200: './assets/weather-icons/animated/thunder.svg', 201: './assets/weather-icons/animated/thunder.svg',
+    202: './assets/weather-icons/animated/thunder.svg', 210: './assets/weather-icons/animated/thunder.svg',
+    211: './assets/weather-icons/animated/thunder.svg', 212: './assets/weather-icons/animated/thunder.svg',
+    221: './assets/weather-icons/animated/thunder.svg', 230: './assets/weather-icons/animated/thunder.svg',
+    231: './assets/weather-icons/animated/thunder.svg', 232: './assets/weather-icons/animated/thunder.svg',
+    300: './assets/weather-icons/animated/rainy-1.svg', 301: './assets/weather-icons/animated/rainy-1.svg',
+    302: './assets/weather-icons/animated/rainy-1.svg', 310: './assets/weather-icons/animated/rainy-1.svg',
+    311: './assets/weather-icons/animated/rainy-1.svg', 312: './assets/weather-icons/animated/rainy-1.svg',
+    313: './assets/weather-icons/animated/rainy-1.svg', 314: './assets/weather-icons/animated/rainy-1.svg',
+    321: './assets/weather-icons/animated/rainy-1.svg', 500: './assets/weather-icons/animated/rainy-6.svg',
+    501: './assets/weather-icons/animated/rainy-6.svg', 502: './assets/weather-icons/animated/rainy-6.svg',
+    503: './assets/weather-icons/animated/rainy-6.svg', 504: './assets/weather-icons/animated/rainy-6.svg',
+    511: './assets/weather-icons/animated/rainy-7.svg', 520: './assets/weather-icons/animated/rainy-7.svg',
+    521: './assets/weather-icons/animated/rainy-7.svg', 522: './assets/weather-icons/animated/rainy-7.svg',
+    531: './assets/weather-icons/animated/rainy-7.svg', 600: './assets/weather-icons/animated/snowy-1.svg',
+    601: './assets/weather-icons/animated/snowy-1.svg', 602: './assets/weather-icons/animated/snowy-1.svg',
+    611: './assets/weather-icons/animated/snowy-1.svg', 612: './assets/weather-icons/animated/snowy-1.svg',
+    613: './assets/weather-icons/animated/snowy-1.svg', 615: './assets/weather-icons/animated/snowy-1.svg',
+    616: './assets/weather-icons/animated/snowy-1.svg', 620: './assets/weather-icons/animated/snowy-1.svg',
+    621: './assets/weather-icons/animated/snowy-1.svg', 622: './assets/weather-icons/animated/snowy-1.svg',
+    701: './assets/weather-icons/animated/cloudy-day-3.svg', 711: './assets/weather-icons/animated/cloudy-day-3.svg',
+    721: './assets/weather-icons/animated/cloudy-day-3.svg', 731: './assets/weather-icons/animated/cloudy-day-3.svg',
+    741: './assets/weather-icons/animated/cloudy-day-3.svg', 751: './assets/weather-icons/animated/cloudy-day-3.svg',
+    761: './assets/weather-icons/animated/cloudy-day-3.svg', 762: './assets/weather-icons/animated/cloudy-day-3.svg',
+    771: './assets/weather-icons/animated/cloudy-day-3.svg', 781: './assets/weather-icons/animated/cloudy-day-3.svg',
+    800: './assets/weather-icons/animated/cloudy-day-1.svg', 801: './assets/weather-icons/animated/cloudy-day-1.svg',
+    802: './assets/weather-icons/animated/cloudy-day-1.svg', 803: './assets/weather-icons/animated/cloudy-day-1.svg',
+    804: './assets/weather-icons/animated/cloudy-day-1.svg',
+}
 
 
 
@@ -31,8 +64,23 @@ document.addEventListener("DOMContentLoaded", function() {
     $('#date-5').text(fiveDays[4])
 
 
+    var cityInStorage = JSON.parse(localStorage.getItem('city'));
+    var buttonText = localStorage.getItem('button');
 
-    geoApiCall(JSON.parse(localStorage.getItem('city'))[JSON.parse(localStorage.getItem('city')).length-1]);
+    //Checks to see if you clicked a button to search for a city or you used the search bar
+    if ( jQuery.inArray(buttonText, cityInStorage) !== -1) {
+
+            geoApiCall(localStorage.getItem('button'));
+
+    } else if (!buttonText) {
+        geoApiCall('houston');
+    }
+    else {
+        geoApiCall(cityInStorage[cityInStorage.length-1]);
+    }
+
+
+    if (cityInStorage) {
 
     //Displays the current saved cities from local storage as buttons
     for (i = 0; i < JSON.parse(localStorage.getItem('city')).length; i++) {
@@ -47,16 +95,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //passes the text from the button to the geo api call
     cityBtns.on('click', function (event) {
-        geoApiCall(event.target.textContent);
-    })
 
+        localStorage.setItem('button', event.target.textContent);
+
+        citySearch();
+    })
+    }
 
   });
 
-//Stores the search value and passes it to the geo api call function
-function citySearch (event) {
 
-    cityName = $('#city-search').val();
+//Stores the search value and passes it to the geo api call function
+function citySearch (city) {
+
+    if ($('#city-search').val() === '') {
+
+        cityName = localStorage.getItem('button');
+        
+    } else {
+        cityName = $('#city-search').val();
+    }
 
     geoApiCall(cityName);
     storeCities(cityName);
@@ -87,6 +145,10 @@ function storeCities (cityName) {
     }
 
     prevCities.push(cityNameUpper);
+    //Removes duplicates from prevCities array
+    let sortedPrevCities = [...new Set(prevCities)];
+    //sets the value of prevCities to the sorted
+    prevCities = sortedPrevCities;
     //Sets city value to the city array
     localStorage.setItem('city', JSON.stringify(prevCities));
 
@@ -129,17 +191,18 @@ function weatherApiCall(lat, lon) {
                 temp.push(Math.round(data.list[i].main.temp) + '\u00B0 Farenheit');
                 weather.push(data.list[i].weather[0].description);
                 wind.push('Wind: ' + Math.round(data.list[i].wind.speed) + 'MPH');
+                iconId.push(data.list[i].weather[0].id);
             }
 
             //passes the data to the display weather function
-            displayWeather(cityName, temp, weather, wind);
+            displayWeather(cityName, temp, weather, wind, iconId);
 
         })
 
 }
 
 //Updates the weather name, temp, description, and wind for each card
-function displayWeather(cityName, temp, weather, wind) {
+function displayWeather(cityName, temp, weather, wind, iconId) {
 
     //Today's forecast must be separate from the loop since there are two cards with the same information
     todayWeather.children('.city-name').text(cityName);
@@ -154,6 +217,7 @@ function displayWeather(cityName, temp, weather, wind) {
         weatherCards[i].children('.weather-info').children('.temp').text(temp[i]);
         weatherCards[i].children('.weather-info').children('.forecast').text(weather[i]);
         weatherCards[i].children('.weather-info').children('.wind').text(wind[i]);
+        weatherCards[i].children('.city-weather-icon').attr('src', weatherIconId[iconId[i]])
     }
 
 }
@@ -164,12 +228,9 @@ function clearCities() {
     localStorage.removeItem('city');
 
 }
-//Go button will initiate the API call
 
-//The API call will automatically update the City name, icon, and weather forecast for all weather cards
-
-//A new city button will display at the top so that you can click and view the previous cities you have viewed
 
 //Add event listener for buttons
 goBtn.on('click', citySearch);
 clearBtn.on('click', clearCities);
+
